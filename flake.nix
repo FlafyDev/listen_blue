51dd515aa73bfc55b0a557d8f929f03dbc24a35f
FlafyDev <flafyarazi@gmail.com>
2022-10-16 21:20:37 +0300

    nix: move to dart-flutter-nix

--- flake.nix
+++ flake.nix
@@ -2,61 +2,30 @@
   description = "A Music Player written in Flutter for Linux";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=2277e4c9010b0f27585eb0bed0a86d7cbc079354";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    # flutter-nix.url = "github:ilkecan/flutter-nix";
-    pub2nix = {
-      url = "github:cir0x/pub2nix";
-      flake = false;
-    };
+    dart-flutter.url = "github:flafydev/dart-flutter-nix";
   };
 
-  outputs = { self, flake-utils, nixpkgs, pub2nix }:
+  outputs = { self, flake-utils, nixpkgs, dart-flutter }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = import nixpkgs {
             inherit system;
+            overlays = [ dart-flutter.overlays.default ];
           };
         in
         {
           packages = rec {
-            listen-blue = pkgs.callPackage ./nix/package.nix {
-              buildFlutterApp = pkgs.callPackage ./nix/build-flutter-app.nix { };
-              mkFlutterApp = pkgs.callPackage ./nix/mk-flutter-app.nix { };
-            };
+            listen-blue = pkgs.callPackage ./nix/package.nix { };
             default = listen-blue;
           };
           devShell = pkgs.mkShell {
             packages = [
-              (pkgs.writeShellScriptBin "pub2nix-lock" ''
-                cp pubspec.lock pub2nix.lock
-                ${pkgs.yq}/bin/yq \
-                  -r \
-                  '.packages[] | select(.source == "hosted") | "\(.description.name) \(.description.url) \(.version)"' \
-                  pub2nix.lock |\
-                while read name url version; do
-                  nix-prefetch-url \
-                    $url/packages/$name/versions/$version.tar.gz \
-                    --name $name-$version \
-                    --type sha256 \
-                    --unpack |\
-                  while read sha256; do
-                    cat <<< $(${pkgs.yq}/bin/yq \
-                      -r \
-                      -y \
-                      --arg name "$name" \
-                      --arg sha256 "$sha256" \
-                      '.packages[$name] += { "sha256": $sha256 }' \
-                      pub2nix.lock) > pub2nix.lock
-                  done
-                done
-              '')
-              (pkgs.callPackage ./nix/sdkdeps2nix.nix { })
+              pkgs.deps2nix
             ];
-
             buildInputs = with pkgs; [
-              # flutter.unwrapped
               at-spi2-core.dev
               clang
               cmake
@@ -94,6 +63,7 @@
         let
           pkgs = import nixpkgs {
             inherit (prev) system;
+            overlays = [ dart-flutter.overlays.default ];
           };
         in
         {
--- nix/package.nix
+++ nix/package.nix
@@ -1,55 +1,14 @@
 { lib
-, flutter
 , buildFlutterApp
-, mkFlutterApp
-, makeWrapper
-, pciutils
 , gst_all_1
 , libunwind
 , elfutils
 , zstd
 , orc
-, cmake
-, cairo
-, SDL2
 }:
-
-# mkFlutterApp {
-#   pname = "listen-blue";
-#   version = "0.0.1";
-#
-#   src = ../.;
-#   vendorHash = "sha256-x6o23pvb2NxAKMhrofYEcLdcrhAHyupvZBGa73MkhwY=";
-#   # vendorHashSha256 = lib.fakeSha256;
-#
-#   nativeBuildInputs = [
-#     gst_all_1.gstreamer
-#     gst_all_1.gst-libav
-#     gst_all_1.gst-plugins-base
-#     gst_all_1.gst-plugins-good
-#     libunwind
-#     elfutils
-#     zstd
-#     orc
-#   ];
-# 
-#   # buildInputs = [
-#   #   gst_all_1.gstreamer
-#   #   gst_all_1.gst-libav
-#   #   gst_all_1.gst-plugins-base
-#   #   gst_all_1.gst-plugins-good
-#   #   libunwind
-#   #   elfutils
-#   #   zstd
-#   #   orc
-#   # ];
-# }
 
 buildFlutterApp {
   pname = "listen_blue";
-  platform = "linux";
-  lockFile = ../pub2nix.lock;
-  specFile = ../pubspec.yaml;
   version = "0.0.1";
 
   src = ../.;
@@ -64,10 +23,6 @@ buildFlutterApp {
     zstd
     orc
   ];
-
-  # postFixup = ''
-  #   wrapProgram $out/bin/guifetch --suffix PATH : ${lib.makeBinPath [ pciutils ]}
-  # '';
 
   meta = with lib; {
     description = "A Music Player written in Flutter for Linux";
